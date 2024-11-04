dac284f79b8fc630b9c605c4a48b4b1d1cfce320
luciyaroshni <75429068+luciyaroshni@users.noreply.github.com>
2024-11-04 19:31:21 +0530

    Update lambda.py

--- lambda.py
+++ lambda.py
@@ -1,3 +1,11 @@
 numbers = int(input("Enter your number"))
 square = lambda num: num * num
 print(square(numbers))
+
+#######
+numbers = int(input("Enter your number"))
+check_even = lambda num : num % 2
+if check_even(numbers) == 0:
+    print("That number is even")
+else:
+    print("That number is odd")
